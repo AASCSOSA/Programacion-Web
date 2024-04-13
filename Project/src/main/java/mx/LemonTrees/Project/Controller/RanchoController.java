d037e23e699c2275225c7e635ba13683f69969ac
Alain Antonio Sosa Cruz <sosacruzalainantonio@gmail.com>
2024-04-13 17:56:33 -0600

    Primer avance
    Relación entre la tabla de rancho y carga

---
+++ Project/src/main/java/mx/LemonTrees/Project/Controller/RanchoController.java
@@ -0,0 +1,68 @@
+package mx.LemonTrees.Project.Controller;
+
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import mx.LemonTrees.Project.Model.Rancho;
+import mx.LemonTrees.Project.Repository.RanchoRepository;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+
+@RestController
+@RequestMapping("/rancho")
+public class RanchoController {
+
+    @Autowired
+    private RanchoRepository ranchoRepository;
+    
+    @GetMapping()
+    public ResponseEntity<Iterable<Rancho>> findAll() {
+        return ResponseEntity.ok(ranchoRepository.findAll());
+    }
+
+    //Crear
+    @PostMapping
+    public ResponseEntity<Void> create(@RequestBody Rancho newRancho, UriComponentsBuilder ucb) {
+        Rancho savedRancho = ranchoRepository.save(newRancho);
+        URI uri = ucb
+                .path("rancho/{Id_Rancho}")
+                .buildAndExpand(savedRancho.getId_Rancho())
+                .toUri();
+        return ResponseEntity.created(uri).build();
+    }
+    
+    //Update
+    @PutMapping("/{Id_Rancho}")
+    public ResponseEntity<Void> update(@PathVariable Long Id_Rancho, @RequestBody Rancho ranchoAct){
+        Rancho ranchoAnt = ranchoRepository.findById(Id_Rancho).get();
+        if (ranchoAnt != null){
+            ranchoAct.setId_Rancho(ranchoAnt.getId_Rancho());
+            ranchoRepository.save(ranchoAct);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+    
+    
+    //Delete
+    @DeleteMapping("/{Id_Rancho}")
+    public ResponseEntity<Void> delete(@PathVariable Long Id_Rancho) {
+        if (ranchoRepository.findById(Id_Rancho).get() != null){
+            ranchoRepository.deleteById(Id_Rancho);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+    
+}
+
+
