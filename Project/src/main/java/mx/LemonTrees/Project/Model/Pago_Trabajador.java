23456acd7a3cb95b331c2de33d477d6378023fea
Felip3Garcia <mac27bross@hotmail.com>
2024-04-18 14:52:15 -0600

    RELACION PAGO_TRABAJADOR Y TRABAJADOR

--- Project/src/main/java/mx/LemonTrees/Project/Model/Pago_Trabajador.java
+++ Project/src/main/java/mx/LemonTrees/Project/Model/Pago_Trabajador.java
@@ -6,6 +6,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.sql.Date;
 
@@ -21,6 +23,10 @@ public class Pago_Trabajador {
 
     @Column(nullable = false)
     private Date Fecha_Pago;
+    
+    @ManyToOne
+    @JoinColumn(name = "Id_Trabajador")
+    private Trabajador trabajador;
 
     public Pago_Trabajador() {
         
