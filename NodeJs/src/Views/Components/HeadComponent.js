132b8933e8cbe292a7ae045a60de172b17689cd3
Alain Antonio Sosa Cruz <sosacruzalainantonio@gmail.com>
2024-05-04 01:53:55 -0600

    Ventana de inicio y navegacion

---
+++ NodeJs/src/Views/Components/HeadComponent.js
@@ -0,0 +1,11 @@
+export default function HeadComponent() {
+    return (
+        <div>
+            <footer>
+                <div>
+                    <h1 id="title-footer">Lemon Tree´s</h1>
+                </div>
+            </footer>
+        </div>
+    );
+}
