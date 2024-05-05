49fb5f5ef59b6a955f7acc5cd832e32d1e5c37cf
PabloBunny2003 <L21TE0296@teziutlan.tecnm.mx>
2024-05-04 23:51:37 -0600

    Avance Controller Venta
    Se modifico el controller de Venta, aunque posiblemente tenga 2 errores

--- Project/src/main/java/mx/LemonTrees/Project/Controller/VentaController.java
+++ Project/src/main/java/mx/LemonTrees/Project/Controller/VentaController.java
@@ -1,12 +1,19 @@
 package mx.LemonTrees.Project.Controller;
 
 
+import mx.LemonTrees.Project.Model.Carga;
+import mx.LemonTrees.Project.Model.Comprador;
 import mx.LemonTrees.Project.Model.Venta;
+import mx.LemonTrees.Project.Repository.CargaRepository;
+import mx.LemonTrees.Project.Repository.CompradorRepository;
 import mx.LemonTrees.Project.Repository.VentaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.Optional;
 
 @RestController
@@ -14,14 +21,20 @@ import java.util.Optional;
 public class VentaController {
     @Autowired
     private VentaRepository ventaRepository;
+    @Autowired
+    private CompradorRepository compradorRepository;
+    @Autowired
+    private CargaRepository cargaRepository;
 
     // Buscar todos
+    @CrossOrigin
     @GetMapping()
     public ResponseEntity<Iterable<Venta>> findAll() {
         return ResponseEntity.ok(ventaRepository.findAll());
     }
 
     // Buscar por ID
+    @CrossOrigin
     @GetMapping("/{Id_Venta}")
     public ResponseEntity<Venta> findById(@PathVariable Integer Id_Venta) {
         Optional<Venta> ventaOptional = ventaRepository.findById(Id_Venta);
@@ -31,32 +44,47 @@ public class VentaController {
         return ResponseEntity.ok(ventaOptional.get());
     }
 
-    // Crear
-    @PostMapping
-    public ResponseEntity<Void> create(@RequestBody Venta newVenta) {
-        Venta savedVenta = ventaRepository.save(newVenta);
-        return ResponseEntity.ok().build();
-    }
+     //Crear
+     @CrossOrigin
+     @PostMapping
+     public ResponseEntity<Void> create(@RequestBody Venta newVenta, UriComponentsBuilder ucb) {
+         Optional<Comprador> compradorOptional = compradorRepository.findById(newVenta.getComprador().getId_Comprador());
+         Optional<Carga> cargaOptional = cargaRepository.findById(newVenta.getCarga().getId_Carga());
+         if (!compradorOptional.isPresent() && !cargaOptional.isPresent()) {
+             return ResponseEntity.unprocessableEntity().build();
+         }
+         newVenta.setComprador(compradorOptional.get());
+         newVenta.setCarga(cargaOptional.get());
+         Venta savedVenta = ventaRepository.save(newVenta);
+         URI uri = ucb
+                 .path("/venta/{Id_Venta}")
+                 .buildAndExpand(savedVenta.getId_Venta())
+                 .toUri();
+         return ResponseEntity.created(uri).build();
 
-    // Actualizar
-    @PutMapping
-    public ResponseEntity<Venta> update(@RequestBody Venta venta) {
-        Optional<Venta> ventaOptional = ventaRepository.findById(venta.getId_Venta());
-        if (!ventaOptional.isPresent()) {
-            return ResponseEntity.notFound().build();
+     }
+
+     //Actualizar
+    @CrossOrigin
+    @PutMapping("/{Id_Venta}")
+    public ResponseEntity<Void> update(@PathVariable Integer Id_Venta, @RequestBody Venta ventaAct){
+        Venta ventaAnt =  ventaRepository.findById(Id_Venta).get();
+        if( ventaAct != null){
+            ventaAct.setId_Venta(ventaAnt.getId_Venta());
+            ventaRepository.save(ventaAct);
+            return ResponseEntity.noContent().build();
         }
-        ventaRepository.save(venta);
-        return ResponseEntity.ok(venta);
+        return ResponseEntity.notFound().build();
     }
 
-    // Eliminar
+    //Eliminar
+    @CrossOrigin
     @DeleteMapping("/{Id_Venta}")
-    public ResponseEntity<Void> delete(@PathVariable Integer Id_Venta) {
-        Optional<Venta> ventaOptional = ventaRepository.findById(Id_Venta);
-        if (!ventaOptional.isPresent()) {
-            return ResponseEntity.notFound().build();
+    public ResponseEntity<Void> delete(@PathVariable Integer Id_Venta){
+        if(ventaRepository.findById(Id_Venta).get() != null){
+            ventaRepository.deleteById(Id_Venta);
+            return ResponseEntity.noContent().build();
         }
-        ventaRepository.deleteById(Id_Venta);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.notFound().build();
     }
 }
